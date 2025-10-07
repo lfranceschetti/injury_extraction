83957886ef66226778c0377cdd7c8b459aede073
lfranceschetti <lfranceschetti@gmx.ch>
2025-10-07 15:37:45 -0300

    pdf extraction

---
+++ constants/columns.py
@@ -0,0 +1,28 @@
+column_names = [
+        "NAME",
+        "TEAM",
+        "CODE",
+        "INJURY_DATE",
+        "RETURN_DATE",
+        "INJURY_LOCATION",
+        "INJURY_SIDE",
+        "INJURY_TYPE",
+        "OCCURRENCE",
+        "OVERUSE_TRAUMA",
+        "ONSET",
+        "CONTACT",
+        "ACTION",
+        "ACTION_DESCRIPTION",
+        "RE_INJURY",
+        "REFEREE_SANCTION",
+        "DIAGNOSTIC_EXAMINATION",
+        "DIAGNOSIS",
+        "SURGERY",
+        "MENSTRUAL_PHASE",
+        "ORAL_CONTRACEPTIVES",
+        "HORMONAL_CONTRACEPTIVES",
+        "OTHER_COMMENTS"
+]
+
+row = {name: "" for name in column_names}
+
